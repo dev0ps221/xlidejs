cc09ec353b136496410c3619477e3606238a4d9d
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-19 17:38:50 +0000

    refactor | document a bit

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
 let slidelist = []
-let imgs = [['1.png','test'],['2.png','image deux'],['3.png','image trois'],['4.png'],'5.png']
-slidelist.push(['#slide-one','slidey',imgs,{previews:1,interval:4000,autoplay:true}])
+let imgs = ['1.png','2.png','3.png','4.png','5.png']
+slidelist.push(['#slide-one','slidey',imgs,{previews:1,interval:8000,autoplay:true}])
 xLides.slideList(slidelist)
