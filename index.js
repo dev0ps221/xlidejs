03de4589a9e751a8b1d09372d16982356d91c458
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-19 18:19:31 +0000

    refactor | document a bit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let slidelist = []
 let imgs = ['1.png','2.png','3.png','4.png','5.png']
 slidelist.push(['#slide-one','slidey',imgs,{previews:1,interval:8000,autoplay:true}])
+slidelist.push(['.car1','car1',imgs,{previews:1,interval:8000,autoplay:true}])
+slidelist.push(['.car2','car2',imgs,{controls:1,vertical:1,interval:4000,autoplay:true}])
+slidelist.push(['.car3','car3',imgs,{previews:1,interval:8000,autoplay:true}])
 xLides.slideList(slidelist)
