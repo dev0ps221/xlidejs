ee5ede5b2a17248ced4382ad299f162e52a6f084
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-19 14:48:42 +0000

    refactor | xLideManager Class

---
+++ example.js
@@ -0,0 +1,4 @@
+let slidelist = []
+let imgs = [['1.png','test'],['2.png','image deux'],'3.png:image trois',['4.png'],'5.png']
+slidelist.push(['#slide-one','slidey',imgs,{interval:2000,controls:1,classList:['vertical'],captions:1,autoplay:true}])
+xLides.slideList(slidelist)
