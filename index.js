c9ff6ba2ce1d0694fc64713c76490be1cca9978d
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-20 16:28:23 +0000

    refactor | galery | xl function

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let slidelist = []
 let imgs = ['1.png','2.png','3.png','6.png','4.png','5.png']
-// slidelist.push(['#slide-one','slidey',imgs,{classList:['bgpurp-a'],interval:4000,autoplay:true}])
-// slidelist.push(['.car1','car1',imgs,{horizontal:1,classList:['bgblue'],interval:2000,autoplay:true}])
-slidelist.push(['.car2','car2',imgs,{vertical:1,classList:['bgpurp-a'],interval:4000,autoplay:true}])
-slidelist.push(['.car3','car3',imgs,{rhorizontal:1,classList:['bgblue'],interval:2000,autoplay:true}])
-xLides.galery('#examplegalery','une galerie simple avec xlide ',['1.png','2.png','3.png','5.png','4.png','2.png','5.png'],{autoplay:1,captions:1,classList:['bgpurp-a'],interval:4000,controls:1})
-xLides.slideList(slidelist)
 
+const examplegalery = xg('examplegalery','une galerie simple avec xlide ',['1.png','2.png','3.png','5.png','4.png','2.png','5.png'],{autoplay:1,captions:1,classList:['bgpurp-a'],interval:4000,controls:1})
+examplegalery.appendTo('#examplegalery')
 
 
 const slideone = xl('slide-one','slidey')
@@ -35,8 +31,3 @@ car3.addImages(imgs)
 car3.addOptions(['interval',2000],'autoplay','horizontal')
 car3.appendTo('.car3')
 
-
-
-const slide = xl()
-slide.addImages([['1.png','img1'],'2.png','3.png','5.png','4.png','2.png','5.png'])
-slide.start()
