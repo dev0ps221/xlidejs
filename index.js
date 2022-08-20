0d56dcc112a1a539dfc39274eac4feb63ed042ce
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-20 11:25:35 +0000

    refactor | galery | xLideGalery

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let slidelist = []
-let imgs = ['1.png','2.png','3.png','4.png','5.png']
+let imgs = ['1.png','2.png','3.png','6.png','4.png','5.png']
 slidelist.push(['#slide-one','slidey',imgs,{classList:['bgpurp-a'],interval:4000,autoplay:true}])
 slidelist.push(['.car1','car1',imgs,{horizontal:1,classList:['bgblue'],interval:2000,autoplay:true}])
 slidelist.push(['.car2','car2',imgs,{vertical:1,classList:['bgpurp-a'],interval:4000,autoplay:true}])
