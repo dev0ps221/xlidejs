02323111dd28bbb1745757cc15594e4e25ae7c89
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-20 10:44:00 +0000

    refactor | galery | xLideGalery

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let slidelist = []
 let imgs = ['1.png','2.png','3.png','4.png','5.png']
 slidelist.push(['#slide-one','slidey',imgs,{classList:['bgpurp-a'],interval:4000,autoplay:true}])
-slidelist.push(['.car1','car1',imgs,{horizontal:1,classList:['bgpurp-a'],interval:2000,autoplay:true}])
+slidelist.push(['.car1','car1',imgs,{horizontal:1,classList:['bgblue'],interval:2000,autoplay:true}])
 slidelist.push(['.car2','car2',imgs,{vertical:1,classList:['bgpurp-a'],interval:4000,autoplay:true}])
-slidelist.push(['.car3','car3',imgs,{rhorizontal:1,classList:['bgpurp-a'],interval:2000,autoplay:true}])
+slidelist.push(['.car3','car3',imgs,{rhorizontal:1,classList:['bgblue'],interval:2000,autoplay:true}])
 xLides.galery('#examplegalery','une galerie simple avec xlide ',['1.png','2.png','3.png','5.png','4.png','2.png','5.png'],{autoplay:1,captions:1,classList:['bgpurp-a'],interval:4000,controls:1})
 xLides.slideList(slidelist)
