0533fd705c9731f860c5886635b756ff2569abb1
dev0ps221 <seybalofficiel@gmail.com>
2023-03-14 19:48:27 +0000

    xlidejs:refactor:lets make some basics functions then put them into and xLideManager object

--- xlide.js
+++ xlide.js
@@ -1,3 +1,21 @@
-function createSlide(target,pictures){
-    
+function xlide(target,options){
+    const slider = target
+    const wrapper = document.createElement('div')
+    let items = slider.querySelectorAll('.xlide-item').length
+    if(items.length){
+
+    }else{
+        if(options.hasOwnProperty('images') && options['images'].length){
+            options['images'].forEach(
+                img=>{
+                    const item = document.createElement('div')
+                    item.classList.add('div')
+                    if((typeof img) === 'string'){
+
+                    }
+                }
+            )
+        }
+    }
+
 }
